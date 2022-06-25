fb8f95d366398d4d4419039dd04b35afca53deb2
Serhii Soldatov <soldatov.own@gmail.com>
2022-06-25 13:39:28 +0300

    change settings

--- medical_clinic/settings.py
+++ medical_clinic/settings.py
@@ -121,8 +121,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+else:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
